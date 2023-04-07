16292f6a8240a97a85021f7f00c10570872dabb8
jyHyun <zex199@naver.com>
2023-04-07 16:11:27 +0900

    Modify 10-8

--- Chapter10/10-8.py
+++ Chapter10/10-8.py
@@ -24,8 +24,6 @@ parent = [0] * (n + 1)  # 부모 테이블 초기화
 edges = []
 result = 0
 
-
-
 # 부모 테이블상에서, 부모를 자기 자신으로 초기화
 for i in range(1, n + 1):
     parent[i] = i
@@ -48,7 +46,5 @@ for edge in edges:
         union_parent(parent, a, b)
         result += cost
         last_cost = cost
-
-
 
 print(result - last_cost)
